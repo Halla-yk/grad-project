e162072158d6710b8ed431efead78f08b66d26b7
Halla-yk <you@example.com>
2022-05-21 14:55:43 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:project/pages/home.dart';
+import 'package:project/test.dart';
+import 'package:camera/camera.dart';
+
+List<CameraDescription>? cameras;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple),
+        debugShowCheckedModeBanner: false,
+        home: Home());
+  }
+}
+/*
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/Halla-yk/grad-project.git
+git push -u origin main*/
